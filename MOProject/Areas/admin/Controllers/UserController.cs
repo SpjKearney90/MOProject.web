89c33f170c76db9516df7748d38ee60c602a79cf
shane kearney <shanepjkearney@gmail.com>
2024-10-16 14:32:47 +0100

    feature/vid7-addtion-dash-screen

---
+++ MOProject/Areas/admin/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MOProject.Areas.admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
