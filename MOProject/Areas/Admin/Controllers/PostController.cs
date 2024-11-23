0a071f0f4b6ed32b65e46592d353cac523f3cfd6
shane kearney <shanepjkearney@gmail.com>
2024-11-23 23:47:17 +0000

    feature-getting there

--- MOProject/Areas/Admin/Controllers/PostController.cs
+++ MOProject/Areas/Admin/Controllers/PostController.cs
@@ -1,187 +1,106 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using MOProject.Data;
 using MOProject.Models;
 using MOProject.ViewModels;
-using X.PagedList.Extensions;
 
-namespace FineBlog.Areas.Admin.Controllers
+namespace MOProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class PostController : Controller
+    [Authorize(Roles = "Admin")]
+    public class SettingController : Controller
     {
         private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
         private readonly IWebHostEnvironment _webHostEnvironment;
-        private readonly UserManager<ApplicationUser> _userManager;
 
-        // Constructor injection
-        public PostController(ApplicationDbContext context,
-                              IWebHostEnvironment webHostEnvironment,
-                              UserManager<ApplicationUser> userManager)
+        public SettingController(ApplicationDbContext context,
+                                INotyfService notyfService,
+                                IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            
             _webHostEnvironment = webHostEnvironment;
-            _userManager = userManager;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Dash(int? page)
+        public async Task<IActionResult> Index()
         {
-            var listOfPosts = await _context.Posts!.Include(x => x.ApplicationUser).ToListAsync();
-            var listOfPostsVM = listOfPosts.Select(x => new PostVM
+            var settings = _context.Settings!.ToList();
+            if (settings.Count > 0)
             {
-                Id = x.Id,
-                Title = x.Title,
-                CreatedDate = x.CreatedDate,
-                ThumbnailUrl = x.ThumbnailUrl,
-                AuthorName = x.ApplicationUser!.FirstName + " " + x.ApplicationUser.LastName
-            }).ToList();
-
-            int pageSize = 5;
-            int pageNumber = page ?? 1;
-
-            return View(listOfPostsVM.OrderByDescending(x => x.CreatedDate).ToPagedList(pageNumber, pageSize));
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View(new CreatePostVM());
+                var vm = new SettingVM()
+                {
+                    Id = settings[0].Id,
+                    SiteName = settings[0].SiteName,
+                    Title = settings[0].Title,
+                    ShortDescription = settings[0].ShortDescription,
+                    ThumbnailUrl = settings[0].ThumbnailUrl,
+                    FacebookUrl = settings[0].FacebookUrl,
+                    InstagramUrl = settings[0].InstagramUrl,
+                 
+                };
+                return View(vm);
+            }
+            var setting = new Setting()
+            {
+                SiteName = "Demo Name",
+            };
+            await _context.Settings!.AddAsync(setting);
+            await _context.SaveChangesAsync();
+            var createdSettings = _context.Settings!.ToList();
+            var createdVm = new SettingVM()
+            {
+                Id = createdSettings[0].Id,
+                SiteName = createdSettings[0].SiteName,
+                Title = createdSettings[0].Title,
+                ShortDescription = createdSettings[0].ShortDescription,
+                ThumbnailUrl = createdSettings[0].ThumbnailUrl,
+                FacebookUrl = createdSettings[0].FacebookUrl,
+               
+            };
+            return View(createdVm);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(CreatePostVM vm)
+        public async Task<IActionResult> Index(SettingVM vm)
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid) { return View(vm); }
+            var setting = await _context.Settings!.FirstOrDefaultAsync(x => x.Id == vm.Id);
+            if (setting == null)
             {
-                // Logging errors for debugging
-                var errors = ModelState.Values.SelectMany(v => v.Errors);
-                foreach (var error in errors)
-                {
-                    Console.WriteLine(error.ErrorMessage);
-                }
+                
                 return View(vm);
             }
-
-            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
-
-            if (loggedInUser == null)
-            {
-                ModelState.AddModelError("", "User not found.");
-                return View(vm);
-            }
-
-            var post = new Post
-            {
-                Title = vm.Title,
-                Description = vm.Description,
-                ShortDescription = vm.ShortDescription,
-                ApplicationUserId = loggedInUser.Id
-            };
-
-            // Generate slug
-            if (!string.IsNullOrWhiteSpace(post.Title))
-            {
-                string slug = vm.Title!.Trim().Replace(" ", "-");
-                post.Slug = slug + "-" + Guid.NewGuid();
-            }
+            setting.SiteName = vm.SiteName;
+            setting.Title = vm.Title;
+            setting.ShortDescription = vm.ShortDescription;
+            setting.FacebookUrl = vm.FacebookUrl;
+            setting.InstagramUrl = vm.InstagramUrl;
 
             if (vm.Thumbnail != null)
             {
-                post.ThumbnailUrl = UploadImage(vm.Thumbnail);
+                setting.ThumbnailUrl = UploadImage(vm.Thumbnail);
             }
-
-            await _context.Posts!.AddAsync(post);
             await _context.SaveChangesAsync();
-
-            return RedirectToAction("Dash", "User", new { area = "Admin" });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == id);
-            if (post != null)
-            {
-                _context.Posts.Remove(post);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Dash", "User", new { area = "Admin" });
-            }
-
-            ModelState.AddModelError("", "Post not found.");
-            return View();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Edit(int id)
-        {
-            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == id);
-            if (post == null)
-            {
-                ModelState.AddModelError("", "Post not found.");
-                return View();
-            }
-
-            var vm = new CreatePostVM
-            {
-                Id = post.Id,
-                Title = post.Title,
-                ShortDescription = post.ShortDescription,
-                Description = post.Description,
-                ThumbnailUrl = post.ThumbnailUrl
-            };
-
-            return View(vm);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Edit(CreatePostVM vm)
-        {
-            if (!ModelState.IsValid)
-            {
-                var errors = ModelState.Values.SelectMany(v => v.Errors);
-                foreach (var error in errors)
-                {
-                    Console.WriteLine(error.ErrorMessage);
-                }
-                return View(vm);
-            }
-
-            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == vm.Id);
-            if (post == null)
-            {
-                ModelState.AddModelError("", "Post not found.");
-                return View();
-            }
-
-            post.Title = vm.Title;
-            post.ShortDescription = vm.ShortDescription;
-            post.Description = vm.Description;
-
-            if (vm.Thumbnail != null)
-            {
-                post.ThumbnailUrl = UploadImage(vm.Thumbnail);
-            }
-
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction("Dash", "User", new { area = "Admin" });
+         
+            return RedirectToAction("Index", "Setting", new { area = "Admin" });
         }
 
         private string UploadImage(IFormFile file)
         {
-            string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            string uniqueFileName = "";
             var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
             var filePath = Path.Combine(folderPath, uniqueFileName);
-            Directory.CreateDirectory(folderPath); // Ensure the directory exists
-
             using (FileStream fileStream = System.IO.File.Create(filePath))
             {
                 file.CopyTo(fileStream);
             }
-
             return uniqueFileName;
         }
     }
