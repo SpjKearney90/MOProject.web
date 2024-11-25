57800251e5bc5cfe320987ac3558a7767921a35b
shane kearney <shanepjkearney@gmail.com>
2024-11-25 21:43:41 +0000

    feature/added errormodel

---
+++ MOProject/ViewModels/HomeVM.cs
@@ -0,0 +1,21 @@
+﻿using MOProject.Models;
+
+namespace MOProject.ViewModels
+{
+    public class HomeVM
+    {
+
+        public string? Title { get; set; }
+
+        public string? ShortDescription { get; set; }
+
+
+        public string? ThumbnailUrl { get; set; }
+
+        public List<Post>? Posts { get; set; }
+
+
+
+
+    }
+}
