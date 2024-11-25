3ec379928594e1d6028fcd6b3c64ee67b96e5a17
shane kearney <shanepjkearney@gmail.com>
2024-11-25 23:29:22 +0000

    feature/added-blog-connection

--- MOProject/Areas/Admin/Controllers/HomeController.cs
+++ MOProject/Areas/Admin/Controllers/HomeController.cs
@@ -1,34 +1,41 @@
-﻿using X.PagedList;
+﻿using MOProject.Data;
+using MOProject.Models;
+using MOProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MOProject.Data;
-using MOProject.ViewModels;
 using System.Diagnostics;
+using X.PagedList;
+using X.PagedList.Extensions;
 
-namespace MOProject.Controllers
+namespace FineBlog.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger,
-                                ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
             _context = context;
         }
 
-        public async Task<IActionResult> Index(int? page)
+        public IActionResult Index(int? page)
         {
             var vm = new HomeVM();
-            var setting = _context.Settings!.ToList();
-            vm.Title = setting[0].Title;
-            vm.ShortDescription = setting[0].ShortDescription;
-            vm.ThumbnailUrl = setting[0].ThumbnailUrl;
+            var settings = _context.Settings!.ToList();
+            vm.Title = settings[0].Title;
+            vm.ShortDescription = settings[0].ShortDescription;
+            vm.ThumbnailUrl = settings[0].ThumbnailUrl;
+
             int pageSize = 4;
             int pageNumber = page ?? 1;
-            vm.Posts = await _context.Posts!.Include(x => x.ApplicationUser).OrderByDescending(x => x.CreatedDate).ToPagedListAsync(pageNumber, pageSize);
+
+            vm.Posts = _context.Posts!
+                               .Include(x => x.ApplicationUser)
+                               .OrderByDescending(x => x.CreatedDate)
+                               .ToPagedList(pageNumber, pageSize);
+
             return View(vm);
         }
 
