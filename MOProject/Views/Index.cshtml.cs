2b3eed0a8b6f4cb182e9053c9d0f07e80de772d8
shane kearney <shanepjkearney@gmail.com>
2024-12-11 19:11:44 +0000

    adjustments

---
+++ MOProject/Views/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MOProject.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
