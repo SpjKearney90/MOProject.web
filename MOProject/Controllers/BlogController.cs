52e89736b36145e002227fe354ce5437b099d82d
shane kearney <shanepjkearney@gmail.com>
2024-12-17 20:36:53 +0000

    blog-retrieval

--- MOProject/Controllers/BlogController.cs
+++ MOProject/Controllers/BlogController.cs
@@ -1,25 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MOProject.Data;
-using System.Runtime.CompilerServices;
+using MOProject.ViewModels;
 
 namespace MOProject.Controllers
 {
     public class BlogController : Controller
     {
-
         private readonly ApplicationDbContext _context;
+        
 
         public BlogController(ApplicationDbContext context)
-
         {
-
             _context = context;
+           
         }
 
-
-        public IActionResult Post()
+        [HttpGet("[controller]/{slug}")]
+        public IActionResult Post(string slug)
         {
-            return View();
+            if (slug == "")
+            {
+
+                return View();
+            }
+            var post = _context.Posts!.Include(x => x.ApplicationUser).FirstOrDefault(x => x.Slug == slug);
+            if (post == null)
+            {
+
+                return View();
+            }
+            var vm = new BlogPostVM()
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorName = post.ApplicationUser!.FirstName + " " + post.ApplicationUser.LastName,
+                CreatedDate = post.CreatedDate,
+                ThumbnailUrl = post.ThumbnailUrl,
+                Description = post.Description,
+                ShortDescription = post.ShortDescription,
+            };
+            return View(vm);
         }
     }
 }
