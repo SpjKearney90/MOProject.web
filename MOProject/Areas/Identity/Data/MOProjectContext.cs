608f5400fa9efcfe6c0f1d8b812011bd2a8192e5
shane kearney <shanepjkearney@gmail.com>
2025-04-09 20:25:38 +0100

    feature/login fixed

---
+++ MOProject/Areas/Identity/Data/MOProjectContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+using MOProject.Areas.Identity.Data;
+using RPProject.Models;
+
+
+
+namespace RPProject.Areas.Identity.Data
+
+{
+
+    public class RPProjectContext : IdentityDbContext<RPProjectUser>
+
+    {
+        public RPProjectContext(DbContextOptions<RPProjectContext> options)
+
+            : base(options)
+
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+
+        {
+
+            base.OnModelCreating(builder);
+
+
+
+            // If needed, configure additional relationships here for future scalability
+
+        }
+
+    }
+
+}
